f3cbae94b83d614d5eb57642e37d229dd8b78a69
tokorom <tokorom@gmail.com>
2014-01-01 17:10:18 +0900

    Add the coverage library

---
+++ GenerateTestCoverageFor_iOS7/TKRGcovFluserTestObserver.m
@@ -0,0 +1,46 @@
+//
+//  TKRGcovFluserTestObserver.m
+//
+//  Created by ToKoRo on 2013-12-05.
+//
+
+#import "TKRGcovFluserTestObserver.h"
+#import <objc/runtime.h>
+
+@interface UIApplication ()
+- (void)gcovFlush;
+@end
+
+@implementation TKRGcovFluserTestObserver
+
+
++ (void)initialize
+{
+    [super initialize];
+    [self.class addGcovFlushMethodToApplication];
+}
+
+//----------------------------------------------------------------------------//
+#pragma mark - XCTestObserver
+//----------------------------------------------------------------------------//
+    
+- (void)stopObserving
+{
+    [super stopObserving];
+    [[UIApplication sharedApplication] gcovFlush];
+}
+
+//----------------------------------------------------------------------------//
+#pragma mark - Private Methods
+//----------------------------------------------------------------------------//
+
++ (void)addGcovFlushMethodToApplication
+{
+    IMP imp = imp_implementationWithBlock(^{
+        extern void __gcov_flush(void);
+        __gcov_flush();
+    });
+    class_addMethod([UIApplication class], @selector(gcovFlush), imp, "v@:");
+}
+
+@end
